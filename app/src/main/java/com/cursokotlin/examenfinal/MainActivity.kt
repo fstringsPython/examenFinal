71346599fe999c7ce5d4b476d7c4a01575f57723
fstrings <alavajaime21@gmail.com>
2024-07-03 23:39:06 -0500

    examen final

--- app/src/main/java/com/cursokotlin/examenfinal/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/examenfinal/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.cursokotlin.examenfinal
 
 import android.app.Activity
+import android.icu.number.IntegerWidth
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.IntegerRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -22,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        viewB.button1.setOnClickListener { calcular() }
+        viewB.button1.setOnClickListener {calcular()}
 
 
 
@@ -33,20 +36,20 @@ class MainActivity : AppCompatActivity() {
         val num1 = viewB.edit1.text.toString().toInt()
         val num2 = viewB.edit2.text.toString().toInt()
         //condicional suma
-        if (viewB.suma.isSelected) {
+        if (viewB.suma.isChecked==true) {
             val resultado = num1 + num2
             viewB.textV1.setText(resultado.toString())
             //condicional resta
         }
-        if (viewB.resta.isSelected) {
+        if (viewB.resta.isChecked==true) {
             val resultado = num1 - num2
             viewB.textV1.setText(resultado.toString())
         }// condicional multiplicacion
-        if (viewB.multiplicacion.isSelected) {
+        if (viewB.multiplicacion.isChecked==true) {
             val resultado = num1 * num2
             viewB.textV1.setText(resultado.toString())
         }// condicional division
-        if (viewB.division.isSelected) {
+        if (viewB.division.isChecked==true) {
             val resultado = num1 / num2
             viewB.textV1.setText(resultado.toString())
         }
